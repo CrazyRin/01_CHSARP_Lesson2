17b06a52931524b69e2561bf923f55b6f17034f5
Crazy Fly <citirin@list.ru>
2024-04-03 22:42:18 +0300

    add Lesson2:Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Создаём массив
+int[] arr = new int[5];
+//Присваиваем значения элементам массива
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 5;
+arr[3] = 4;
+arr[4] = 3;
+//Выводим каждый элемент массива построчно
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+//Console.WriteLine(arr[5]); (Будет ошибка, т.к. это Обращение к несуществующему индексу массива)
+
+
+//Создаём новый массив
+int[] arr2 = new int[5] {1, 2, 3, 4, 5};
+
+//Создадим 3-й массив упращенной саписью
+int[] arr3 = {1, 2, 3, 4, 5};
